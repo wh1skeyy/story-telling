117928dd9f3543e4b678b1981dcad22eee1eed98
sh0chu <150662814+sh0chu@users.noreply.github.com>
2025-04-13 17:50:44 +0000

    modified:   EDA_MLE.ipynb 	modified:   file-seperater.py

--- file-seperater.py
+++ file-seperater.py
@@ -1,9 +1,9 @@
 import pandas as pd
-data = 'cleaned-data.csv'
-data = pd.read_csv(data)
+def separate_roles(input_file):
+    data = pd.read_csv(input_file)
 
-ml_engineer_df = data[data['Q5'] == 'Machine Learning Engineer']
-ml_engineer_df.to_csv('machine_learning_engineers.csv', index=False)
+    ml_engineer_df = data[data['Q5'] == 'Machine Learning Engineer']
+    ml_engineer_df.to_csv('machine_learning_engineers.csv', index=False)
 
-dba_df = data[data['Q5'] == 'DBA/Database Engineer']
-dba_df.to_csv('dba_database_engineers.csv', index=False)
+    dba_df = data[data['Q5'] == 'DBA/Database Engineer']
+    dba_df.to_csv('dba_database_engineers.csv', index=False)
