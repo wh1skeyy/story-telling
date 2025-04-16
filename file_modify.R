37a665bddf7f03966e5a2974a661807435490853
sh0chu <150662814+sh0chu@users.noreply.github.com>
2025-04-16 16:19:43 +0000

    modified:   Data_Cleaning_and_Processing.ipynb 	modified:   EDA_MLE.ipynb 	modified:   __pycache__/file_seperater.cpython-312.pyc 	modified:   file_modify.R

--- file_modify.R
+++ file_modify.R
@@ -2,10 +2,10 @@
 library(dplyr)
 
 # Read the CSV file into a dataframe
-data <- read.csv("Data_Storage/kaggle_survey_2020_responses.csv", stringsAsFactors = FALSE)
+data <- read.csv("Data_Storage\\kaggle_survey_2020_responses.csv", stringsAsFactors = FALSE)
 
 # Replace all occurrences of 'None' with 'No/None'
 data <- data %>% mutate_all(~ gsub("None", "No/None", .))
 
 # Save the modified dataframe back to a CSV file (optional)
-write.csv(data, "kaggle_survey_2020_responses_modified.csv", row.names = FALSE)
+write.csv(data, "Data_Storage\\kaggle_survey_2020_responses_modified.csv", row.names = FALSE)
